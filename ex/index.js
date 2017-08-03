913e6104a31b698eeaa9fb6a131cf489baccfb3e
kizylyrio <kizylirio@gmail.com>
2017-08-03 10:10:43 -0300

    Add react

--- ex/index.js
+++ ex/index.js
@@ -1,12 +1,14 @@
-import LoggerEchma from './loggerEcmaScript'
+//*************logger************** */
+// import LoggerEchma from './loggerEcmaScript'
 
-const logger = new LoggerEchma('Using EcmaScript')
-console.log(logger.toString());
+// const logger = new LoggerEchma('Using EcmaScript')
+// console.log(logger.toString());
 
 // const logger = require('./loggerCommonJs')
 // logger.info('Using CommonJS!');
 
-import clone from './utils'
+//************utils************* */
+//import clone from './utils'
 
 // //create object
 // const product = {
@@ -19,3 +21,12 @@ import clone from './utils'
 // newProduct.name = 'Bic - Black'
 
 // console.log(product, newProduct)
+
+//************react Hello World************ */
+import 'react'
+
+export default props =>(
+    <h1>Hello World</h1>
+)
+
+console.log("Hello World")
