9c85a76351cb9f39c398042318f0d123c3af31b1
kizylyrio <kizylirio@gmail.com>
2017-08-11 09:05:18 -0300

    Child tag

--- hello-react/ex/family.jsx
+++ hello-react/ex/family.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import Member from './member'
 
-export default () => (
+export default props => (
     <div>
-        <Member name="João" lastName="Silva" />
-        <Member name="Maria" lastName="Silva" />
-        <Member name="José" lastName="Silva" />
-        <Member name="Ana" lastName="Silva" />
+        {props.children}
     </div>
 )
